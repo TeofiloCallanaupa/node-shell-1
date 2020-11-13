be88bb69459712b97bba49d792915c48133c042b
Teofilo Callanaupa <teofilocallanaupa@gmail.com>
2020-11-13 16:26:35 -0500

    dooone

--- bash.js
+++ bash.js
@@ -1,11 +1,32 @@
-process.stdout.write('prompt > ');
+process.stdout.write("prompt > ");
+const pwd = require("./pwd");
+const ls = require("./ls");
+const cat = require("./cat");
+const curl = require("./curl");
 
-process.stdin.on('data', (data) => {
+const fs = require("fs");
+
+process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
-  } else {
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\npropmt > ');
+  if (cmd === "pwd") {
+    pwd();
+    done();
+  }
+  if (cmd === "ls") {
+    ls();
+    done();
+  }
+  if (cmd.split(" ")[0] === "cat") {
+    cat(cmd.split(" ")[1]);
+    done();
+  }
+  if (cmd.split(" ")[0] === "curl") {
+    curl(cmd.split(" ")[1]);
+    done();
   }
 });
+
+const done = () => {
+  // process.stdout.write("\n");
+  process.stdout.write("\nprompt > ");
+};
---
+++ curl.js
@@ -0,0 +1,9 @@
+const request = require ('request');
+
+module.exports = function (url) {
+    request(url,function(error, response, body){
+        console.error('error: ', error);
+        console.log("statusCode: ", response && response.statusCode);
+        console.log('body: ', body);
+    })
+}
